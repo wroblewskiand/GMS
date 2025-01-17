b4253bbea5746206c3987916cafcd5062d84306a
Andrzej Wroblewski <wroblewski.and@gmail.com>
2025-01-17 16:03:52 +0100

    zaliczony test na STOS

--- GMS.cpp
+++ GMS.cpp
@@ -1,7 +1,4 @@
-﻿// GMS.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 #include <cmath>
 #include <queue>
@@ -10,7 +7,6 @@ using namespace std;
 
 int bfsOdleglosc(const vector<vector<int>>& graph, int start);
 
-// Lista sąsiedztwa do przechowywania grafu
 vector<vector<int>> listaSasiedztwa;
 vector<int> wezlySymetrii;
 
@@ -45,67 +41,62 @@ void barabasiRavaszVicsek(int n) {
     // Inicjalizacja listy sąsiedztwa dla kroku >=2
     for (int i = 2; i <= n; i++) {
 
-        tempListaSasiedztwa = listaSasiedztwa;  // Kopiowanie węzłów z poprzedniej iteracji do tymczasowej listy
+        // Kopiowanie węzłów z poprzedniej iteracji do tymczasowej listy
+        tempListaSasiedztwa = listaSasiedztwa;  
+
         tempDolneWezly.clear();
-        int wielkoscJednostki = listaSasiedztwa.size();
-        //int wielkoscJednostki = pow(3, i - 1);
+        int wielkoscKopii = listaSasiedztwa.size();
 
         // Dodawanie dwóch jednostek
-        for (int jednostkaID = 1; jednostkaID <= 2; jednostkaID++) {
+        for (int nrKopi = 1; nrKopi <= 2; nrKopi++) {
 
-            int przesuniecie = wielkoscJednostki * jednostkaID;
-            //int przesuniecie = tempListaSasiedztwa.size() * jednostkaID;
+            int przesuniecie = wielkoscKopii * nrKopi;
 
-            // dodawanie nowej jednostki
-            for (int wezel = 0; wezel < wielkoscJednostki; wezel++) {
-                listaSasiedztwa.push_back({});
-            }
+            // dodawanie nowej jednostki, przechodzenie po węzłach grafu kopiowanego
+            for (int wezel = 0; wezel < wielkoscKopii; wezel++) {
 
-            for (int wezel = 0; wezel < wielkoscJednostki; wezel++) {
-
-                int aktualnyWezel = wezel + przesuniecie;
-                for (int sasiad = 0; sasiad < tempListaSasiedztwa[wezel].size(); sasiad++) {
-
-                    int aktualnySasiad = tempListaSasiedztwa[wezel][sasiad] + przesuniecie;
-
-                    listaSasiedztwa[aktualnyWezel].push_back(aktualnySasiad);
-                    listaSasiedztwa[aktualnySasiad].push_back(aktualnyWezel);
-
-                    if (jednostkaID == 1) {
-                        wezlySymetrii.push_back(listaSasiedztwa.size());
-                    }
+                vector<int> sasiedztwoWezla;
+                for (int sasiad : listaSasiedztwa[wezel]) {
+                    sasiedztwoWezla.push_back(sasiad + przesuniecie);
                 }
 
+                if (nrKopi == 1) {
+                    wezlySymetrii.push_back(tempListaSasiedztwa.size());
+                }
+                
+                tempListaSasiedztwa.push_back(sasiedztwoWezla);
             }
             // Łączenie z korzeniem
-            for (int wezel : dolneWezly) {
-                listaSasiedztwa[0].push_back(wezel + przesuniecie);
-                listaSasiedztwa[wezel + przesuniecie].push_back(0);
-                tempDolneWezly.push_back(wezel + przesuniecie);
+            for (int dolnyWezel : dolneWezly) {
+                int przesuniecieDolnychWezlow = dolnyWezel + przesuniecie;
+                tempListaSasiedztwa[0].push_back(przesuniecieDolnychWezlow);
+                tempListaSasiedztwa[przesuniecieDolnychWezlow].push_back(0);
+                tempDolneWezly.push_back(przesuniecieDolnychWezlow);
             }
         }
         dolneWezly = tempDolneWezly;
+        listaSasiedztwa = tempListaSasiedztwa;
     }
 }
 
 
-int bfsOdleglosc(const vector<vector<int>>& listaSasiedztwa, int start) {
-    int n = listaSasiedztwa.size();                   // liczba wierzchołków
-    vector<int> odleglosci(n, -1);           // tablica odległości (-1 oznacza nieosiągalne)
-    queue<int> q;                           // kolejka BFS
+int bfsOdleglosc(const vector<vector<int>>& graf, int start) {
+    int n = graf.size();                // liczba wierzchołków
+    vector<int> odleglosci(n, -1);      // tablica odległości (-1 oznacza nieosiągalne)
+    queue<int> q;                       // kolejka BFS
 
-    int suma = 0;                          // suma wszystkich odległości
-    odleglosci[start] = 0;                   // odległość do siebie samego to 0
+    int suma = 0;                       // wyzerowanie odległości
+    odleglosci[start] = 0;              // wyzerowanie licznika
     q.push(start);
 
     while (!q.empty()) {
         int wezel = q.front();
         q.pop();
 
-        for (int neighbor : listaSasiedztwa[wezel]) {
-            if (odleglosci[neighbor] == -1) { // jeśli sąsiad nie został jeszcze odwiedzony
-                odleglosci[neighbor] = odleglosci[wezel] + 1; // Odległość to odległość bieżącego + 1
-                suma += odleglosci[neighbor]; // zwiększamy sumę odległości
+        for (int neighbor : graf[wezel]) {
+            if (odleglosci[neighbor] == -1) {                   // jeśli sąsiad nie został jeszcze odwiedzony
+                odleglosci[neighbor] = odleglosci[wezel] + 1;   // Odległość to odległość bieżącego + 1
+                suma += odleglosci[neighbor];                   // zwiększamy sumę odległości
                 q.push(neighbor);
             }
         }
@@ -113,81 +104,40 @@ int bfsOdleglosc(const vector<vector<int>>& listaSasiedztwa, int start) {
     return suma;
 }
 
-
-// Funkcja do obliczania najkrótszej ścieżki między dwoma węzłami
-//int bfs(int start, int end) {
-//    vector<bool> odwiedzone(listaSasiedztwa.size(), false);
-//    vector<int> odleglosc(listaSasiedztwa.size(), 0);
-//    queue<int> kolejka;
-//
-//    odwiedzone[start] = true;
-//    kolejka.push(start);
-//
-//    while (!kolejka.empty()) {
-//        int v = kolejka.front();
-//        kolejka.pop();
-//
-//        for (int sasiad : listaSasiedztwa[v]) {
-//            if (!odwiedzone[sasiad]) {
-//                odwiedzone[sasiad] = true;
-//                odleglosc[sasiad] = odleglosc[v] + 1;
-//                kolejka.push(sasiad);
-//
-//                if (sasiad == end) {
-//                    return odleglosc[end];
-//                }
-//            }
+//void wypiszGraf(const vector<vector<int>>& graf) {
+//    for (int i = 0; i < graf.size(); i++) {
+//        cout << i << ": ";
+//        for (int sasiad : graf[i]) {
+//            cout << sasiad << " ";
 //        }
+//        cout << endl;
 //    }
-//    return -1; // Nie ma ścieżki między węzłami
-//}
-
-//int obliczSumeOdleglosci(const vector<vector<int>>& listaSasiedztwa) {
-//    int sumaOdleglosci = 0;
-//    for (int i = 0; i < listaSasiedztwa.size(); i++) {
-//        for (int j = i + 1; j < listaSasiedztwa.size(); j++) {
-//            sumaOdleglosci += bfs(i, j);
-//        }
-//    }
-//    return sumaOdleglosci;
 //}
 
 
-void wypiszGraf(const vector<vector<int>>& graf) {
-    for (int i = 0; i < graf.size(); i++) {
-        cout << i << ": ";
-        for (int sasiad : graf[i]) {
-            cout << sasiad << " ";
-        }
-        cout << endl;
-    }
-}
+//int main() {
+//
+//    for (int i = 4; i <= 8; i++) {
+//
+//        // Implementacja modelu Barabasi–Ravasz–Vicsek
+//        barabasiRavaszVicsek(i);
+//
+//        // Obliczanie sumy odległości między wszystkimi parami węzłów
+//        int sumaOdleglosci = bfsOdleglosc(listaSasiedztwa, 0) / 2;
+//
+//        for (int wezel : wezlySymetrii) {
+//            sumaOdleglosci += bfsOdleglosc(listaSasiedztwa, wezel);
+//        }
+//
+//        cout << i << ": " << sumaOdleglosci << endl;
+//        //wypiszGraf(listaSasiedztwa);
+//        cout << endl;
+//    }
+//    return 0;
+//}
 
 
-int main() {
 
-    for (int i = 2; i <= 3; i++) {
-
-        // Implementacja modelu Barabasi–Ravasz–Vicsek
-        barabasiRavaszVicsek(i);
-
-        // Obliczanie sumy odległości między wszystkimi parami węzłów
-        int sumaOdleglosci = bfsOdleglosc(listaSasiedztwa, 0) / 2;
-
-        for (int wezel : wezlySymetrii) {
-            sumaOdleglosci += bfsOdleglosc(listaSasiedztwa, wezel);
-        }
-
-        cout << i << ": " << sumaOdleglosci << endl;
-        wypiszGraf(listaSasiedztwa);
-        cout << endl;
-    }
-    return 0;
-}
-
-
-/*
-*
 int main() {
     int model, parametr1 = 0, parametr2;
 
@@ -205,43 +155,43 @@ int main() {
             // Implementacja modelu Barabasi–Ravasz–Vicsek
             barabasiRavaszVicsek(parametr1);
 
-            cout << listaSasiedztwa.size() << endl;
+            //Obliczanie sumy odległości między wszystkimi parami węzłów
+            int sumaOdleglosci = bfsOdleglosc(listaSasiedztwa, 0) / 2;
+                
+            for (int wezel : wezlySymetrii) {
+                sumaOdleglosci += bfsOdleglosc(listaSasiedztwa, wezel);
+            }
+            cout << sumaOdleglosci << endl;
             break;
-
+        /*
         case 1:
             // Implementacja modelu Lu–Su–Guo
+            cout << 0 << endl;
             break;
         case 2:
             // Implementacja modelu Simplical
+            cout << 0 << endl;
             break;
         case 3:
             // Implementacja modelu Wzrostowo-iteracyjnego
             cin >> parametr2; // Wczytanie parametru r
+            cout << 0 << endl;
             break;
         case 4:
             // Implementacja modelu DCN
+            cout << 0 << endl;
             break;
         case 5:
             // Implementacja modelu GFG
+            cout << 0 << endl;
             break;
         case 6:
             // Implementacja modelu Kronecker
+            cout << 0 << endl;
             break;
+        */
         }
     }
 
     return 0;
 }
-// */
-
-
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
