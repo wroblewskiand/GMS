16c81a6a0f791bc69ee5a0bd96c0d40160e3906c
Andrzej Wroblewski <wroblewski.and@gmail.com>
2025-01-18 13:09:06 +0100

    zaliczony test LuSuGuo na STOS

--- GMS.cpp
+++ GMS.cpp
@@ -8,19 +8,18 @@ using namespace std;
 int bfsOdleglosc(const vector<vector<int>>& graph, int start);
 
 vector<vector<int>> listaSasiedztwa;
-vector<int> wezlySymetrii;
+vector<int> wektorSymetrii;
 
 
 void barabasiRavaszVicsek(int n) {
 
     listaSasiedztwa.clear();
-    wezlySymetrii.clear();
+    wektorSymetrii.clear();
 
     vector<vector<int>> tempListaSasiedztwa;
     vector<int> dolneWezly;
     vector<int> tempDolneWezly;
-    vector<int> wektorSymetrii;
-
+    
 
     // Inicjalizacja listy sąsiedztwa dla kroku 0 (pojedynczy węzeł)
     listaSasiedztwa.push_back({});  // Korzeń
@@ -34,7 +33,7 @@ void barabasiRavaszVicsek(int n) {
         listaSasiedztwa[i].push_back(0);
         dolneWezly.push_back(i);
     }
-    wezlySymetrii.push_back(1);
+    wektorSymetrii.push_back(1);
     if (n == 1) { return; }
 
 
@@ -61,7 +60,7 @@ void barabasiRavaszVicsek(int n) {
                 }
 
                 if (nrKopi == 1) {
-                    wezlySymetrii.push_back(tempListaSasiedztwa.size());
+                    wektorSymetrii.push_back(tempListaSasiedztwa.size());
                 }
                 
                 tempListaSasiedztwa.push_back(sasiedztwoWezla);
@@ -79,10 +78,63 @@ void barabasiRavaszVicsek(int n) {
     }
 }
 
+void luSuGuo(int wezel) {
+
+    listaSasiedztwa.clear();
+    if (wezel == 0) return;
+
+    // 1 wezel
+    listaSasiedztwa.push_back({});
+    if (wezel == 1) return;
+
+    // 2 wezly
+    listaSasiedztwa[0].push_back({ 1 });
+    listaSasiedztwa.push_back({ 0 });
+    if (wezel == 2) return;
+
+    // 3 wezly
+    listaSasiedztwa[0].push_back({ 2 });
+    listaSasiedztwa[1].push_back({ 2 });
+    listaSasiedztwa.push_back({ 0,1 });
+    if (wezel == 3) return;
+
+    // kolejne wezly
+    
+    for (int i=3; i <= wezel; i++) {
+        
+        int brat, rodzic, przodek, krok, warunek;
+
+        // sąsiedtwo z przodkiem
+        krok = 0;
+        warunek = 0;
+        while (i > warunek) {
+            krok++;
+            warunek += pow(2, krok);
+        }
+        przodek = floor((i - 1) / 2);
+        przodek = przodek % (krok - 1);
+        listaSasiedztwa.push_back({ przodek });
+        listaSasiedztwa[przodek].push_back({ i });
+  
+        // sąsiedztwo z rodzicem
+        rodzic = floor((i - 1) / 2);
+        listaSasiedztwa[i].push_back({rodzic});
+        listaSasiedztwa[rodzic].push_back({ i });
+    
+
+        // sąsiedztwo z bratem
+        if ((i & 1) == 0) {
+            brat = i - 1;
+            listaSasiedztwa[i].push_back({brat});
+            listaSasiedztwa[brat].push_back({ i });
+        }
+        // cout << i << ": " << krok << endl;
+    }
+}
 
 int bfsOdleglosc(const vector<vector<int>>& graf, int start) {
     int n = graf.size();                // liczba wierzchołków
-    vector<int> odleglosci(n, -1);      // tablica odległości (-1 oznacza nieosiągalne)
+    vector<int> odleglosci(n, -1);      // wektor odleglosci o wielkości n, zainicjalizowany samymi -1 (-1 oznacza nieosiągalne)
     queue<int> q;                       // kolejka BFS
 
     int suma = 0;                       // wyzerowanie odległości
@@ -104,42 +156,52 @@ int bfsOdleglosc(const vector<vector<int>>& graf, int start) {
     return suma;
 }
 
-//void wypiszGraf(const vector<vector<int>>& graf) {
-//    for (int i = 0; i < graf.size(); i++) {
-//        cout << i << ": ";
-//        for (int sasiad : graf[i]) {
-//            cout << sasiad << " ";
-//        }
-//        cout << endl;
-//    }
-//}
+//*
+void wypiszGraf(const vector<vector<int>>& graf) {
+    for (int i = 0; i < graf.size(); i++) {
+        cout << i << ": ";
+        for (int sasiad : graf[i]) {
+            cout << sasiad << " ";
+        }
+        cout << endl;
+    }
+}//*/
 
+/*
+int main() {
 
-//int main() {
-//
-//    for (int i = 4; i <= 8; i++) {
-//
-//        // Implementacja modelu Barabasi–Ravasz–Vicsek
-//        barabasiRavaszVicsek(i);
-//
-//        // Obliczanie sumy odległości między wszystkimi parami węzłów
-//        int sumaOdleglosci = bfsOdleglosc(listaSasiedztwa, 0) / 2;
-//
-//        for (int wezel : wezlySymetrii) {
-//            sumaOdleglosci += bfsOdleglosc(listaSasiedztwa, wezel);
-//        }
-//
-//        cout << i << ": " << sumaOdleglosci << endl;
-//        //wypiszGraf(listaSasiedztwa);
-//        cout << endl;
-//    }
-//    return 0;
-//}
+    for (int i = 9; i <= 9; i++) {
 
+        // Implementacja modelu Barabasi–Ravasz–Vicsek
+        //barabasiRavaszVicsek(i);
+        int a = 348;
+        // Implementacja modelu Lu–Su–Guo
+        luSuGuo(a-1);
 
+        int sumaCalkowita = 0;
+        for (int i = 0; i < listaSasiedztwa.size(); ++i) {
+            sumaCalkowita += bfsOdleglosc(listaSasiedztwa, i);
+        }
 
+         //Obliczanie sumy odległości między wszystkimi parami węzłów
+        //int sumaOdleglosci = bfsOdleglosc(listaSasiedztwa, 0) ;
+       
+        //for (int wezel : wektorSymetrii) {
+        //    sumaOdleglosci += bfsOdleglosc(listaSasiedztwa, wezel);
+        //}
+       
+        cout << i << ": " << sumaCalkowita/2 << endl;
+        // wypiszGraf(listaSasiedztwa);
+        cout << endl;
+    }
+    return 0;
+}
+//*/
+
+//*
 int main() {
     int model, parametr1 = 0, parametr2;
+    int sumaOdleglosci;
 
     while (cin >> model) {
         // Wczytywanie parametrów modelu
@@ -156,42 +218,49 @@ int main() {
             barabasiRavaszVicsek(parametr1);
 
             //Obliczanie sumy odległości między wszystkimi parami węzłów
-            int sumaOdleglosci = bfsOdleglosc(listaSasiedztwa, 0) / 2;
+            sumaOdleglosci = bfsOdleglosc(listaSasiedztwa, 0) / 2;
                 
-            for (int wezel : wezlySymetrii) {
+            for (int wezel : wektorSymetrii) {
                 sumaOdleglosci += bfsOdleglosc(listaSasiedztwa, wezel);
             }
             cout << sumaOdleglosci << endl;
             break;
-        /*
         case 1:
             // Implementacja modelu Lu–Su–Guo
-            cout << 0 << endl;
+            luSuGuo(parametr1-1);
+
+            //Obliczanie sumy odległości między wszystkimi parami węzłów
+            int sumaCalkowita = 0;
+            for (int i = 0; i < listaSasiedztwa.size(); ++i) {
+                sumaCalkowita += bfsOdleglosc(listaSasiedztwa, i);
+            }
+            cout << sumaCalkowita/2 << endl;
             break;
-        case 2:
-            // Implementacja modelu Simplical
-            cout << 0 << endl;
-            break;
-        case 3:
-            // Implementacja modelu Wzrostowo-iteracyjnego
-            cin >> parametr2; // Wczytanie parametru r
-            cout << 0 << endl;
-            break;
-        case 4:
-            // Implementacja modelu DCN
-            cout << 0 << endl;
-            break;
-        case 5:
-            // Implementacja modelu GFG
-            cout << 0 << endl;
-            break;
-        case 6:
-            // Implementacja modelu Kronecker
-            cout << 0 << endl;
-            break;
-        */
+        
+        //case 2:
+        //    // Implementacja modelu Simplical
+        //    cout << 0 << endl;
+        //    break;
+        //case 3:
+        //    // Implementacja modelu Wzrostowo-iteracyjnego
+        //    cin >> parametr2; // Wczytanie parametru r
+        //    cout << 0 << endl;
+        //    break;
+        //case 4:
+        //    // Implementacja modelu DCN
+        //    cout << 0 << endl;
+        //    break;
+        //case 5:
+        //    // Implementacja modelu GFG
+        //    cout << 0 << endl;
+        //    break;
+        //case 6:
+        //    // Implementacja modelu Kronecker
+        //    cout << 0 << endl;
+        //    break;
+        
         }
     }
-
     return 0;
 }
+//*/
